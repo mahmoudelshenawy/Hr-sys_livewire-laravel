3ac8136178a8ab3a75a390d62155081a79e2ce7c
MG_elshenawy <mahmoud.gamal1884@gmail.com>
2021-02-08 00:59:00 +0200

    first initialization

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
 
-use App\Http\Livewire\Layout\BanksPage;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
 
-    Route::get('/', function () {
+    Route::get('/home', function () {
         return view('home');
     });
-
     Route::resource('users', App\Http\Controllers\UsersController::class);
     Route::resource('app_settings', App\Http\Controllers\AppSettingsController::class);
     Route::resource('business_settings', App\Http\Controllers\MainOfficeSettingController::class);
